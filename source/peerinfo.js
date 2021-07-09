3f860eb3f756579f3fad4fb6cc2c9ab09b2a92d1
Tristan B. Kildaire <deavmi@redxen.eu>
2021-07-09 21:30:13 +0200

    WIP: Error handling on `fetch()`n failure

--- source/peerinfo.js
+++ source/peerinfo.js
@@ -18,12 +18,22 @@ var app = new Vue(
 async function updateData()
 {
 	resp = await fetch("http://[201:6c56:f9d5:b7a5:8f42:b1ab:9e0e:5169]:9091/api/peerinfo?key="+key)
-	resp = await resp.json();
-    console.log(resp.name);
-	app.address = resp.address;
-    app.domain = resp.domain;
-    app.location = resp.location;
-    app.name = resp.name;
+    if(resp != null)
+    {
+        resp = await resp.json();
+        console.log(resp.name);
+        app.address = resp.address;
+        app.domain = resp.domain;
+        app.location = resp.location;
+        app.name = resp.name;
+
+        /* TODO: Set last updated bubble */
+    }
+    else
+    {
+        /* TODO: Set a bubble here */
+    }
+	
 }
 
 // Add this to the event loop with a 500millisecond re-run interval
