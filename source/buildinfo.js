8385d171ea8f6043d52f488e0d27715e21ddd1ab
Tristan B. Kildaire <deavmi@redxen.eu>
2021-07-10 15:30:22 +0200

    WIP: Make buildinfo dynamic
    Added buildinfo.js and buildinfo.dt

---
+++ source/buildinfo.js
@@ -0,0 +1,72 @@
+var app = new Vue(
+	{
+		el: "#render",
+		data: {
+			architecture: "<address>",
+            buildname: "<domain>",
+            version: "<location>",
+            platform: "<name>",
+		},
+		methods: {
+			refresh: async function(event) {
+				updateData();
+			}
+		}
+	}
+)
+
+async function updateData()
+{
+	resp = await fetch("/api/buildinfo?key="+key)
+
+    if ("location" in resp)
+    {
+        console.log("naaia");
+    }
+
+    if(resp != null)
+    {
+        /* Get the JSON */
+        resp = await resp.json();
+
+        /* Get status */
+        var status = resp.status;
+
+        /* If wellformed */
+        if(status == "ok")
+        {
+            app.address = resp.response.address;
+            app.domain = resp.response.domain;
+            app.location = resp.response.location;
+            app.name = resp.response.name;
+            app.nodeinfo = resp.response.nodeinfo;
+            app.contact = resp.response.contact;
+
+            /* TODO: Set bubble here */
+        }
+        /* If malformed */
+        else
+        {
+            /* TODO: Set bubble here */
+        }
+        
+    
+    
+
+        /* TODO: Set last updated bubble */
+    }
+    else
+    {
+        /* TODO: Set a bubble here */
+        app.address = "Failed to fetch";
+        app.domain = "Failed to fetch";
+        app.location = "Failed to fetch";
+        app.name = "Failed to fetch";
+        app.nodeinfo = "Failed to fetch";
+        app.contact = "Failed to fetch";
+    }
+	
+}
+
+// Add this to the event loop with a 500millisecond re-run interval
+setInterval(updateData, 500);
